cd3b78fea158d5ffcdfd6fb86daa60510319c595
Danny Goodman <goodman.danny@gmail.com>
2011-01-12 14:41:37 -0800

    commented out sparsity stuff

--- sparseAutoencoderCost.m
+++ sparseAutoencoderCost.m
@@ -26,10 +26,6 @@ b2 = theta(2*hiddenSize*visibleSize+hiddenSize+1:end);
 % b1grad = zeros(size(b1)); 
 % b2grad = zeros(size(b2));
 
-avgactivations = hiddenvalues * transpose(weightsbuffer) / numpatches; % hiddensize * 1
-sparsityvec = beta * (-sparsityParam ./ avgactivations + (1 - sparsityParam) ./ (1 - avgactivations)); % hiddensize * 1
-sparsityvec * weightsbuffer
-
 %% ---------- YOUR CODE HERE --------------------------------------
 %  Instructions: Compute the cost/optimization objective J_sparse(W,b) for the Sparse Autoencoder,
 %                and the corresponding gradients W1grad, W2grad, b1grad, b2grad.
@@ -69,8 +65,9 @@ b2grad = delta3 * transpose(weightsbuffer) / numpatches; % visiblesize * 1, aver
 
 % % Sparsity stuff
 % avgactivations = hiddenvalues * transpose(weightsbuffer) / numpatches; % hiddensize * 1
-% sparsityvec = beta * (-sparsityParam ./ avgactivations + (1 - sparsityParam) ./ (1 - avgactivations)); % hiddensize * 1
+% sparsityvec = -sparsityParam ./ avgactivations + (1 - sparsityParam) ./ (1 - avgactivations); % hiddensize * 1
 % sparsityvec * weightsbuffer % Add this to the delta2 parenthesis
+% kldiv = sparsityParam * log(prod(sparsityParam ./ avgactivations)) + (1 - sparsityParam) * log(prod( (1 - sparsityParam) ./ (1 - avgactivations) )); % Add this to cost
 
 delta2 = (transpose(W2) * delta3) .* hiddenvalues .* (1 - hiddenvalues); % hiddensize * numpatches
 W1grad = delta2 * transpose(data) / numpatches; % hiddensize * visiblesize, averaged over all patches
